3b293be090a953abe371288feeccf422cbf37c74
admlinux <admlinux@portulan.com.br>
2016-09-29 14:02:21 -0300

    Extração do PAYLOAD e comentários

--- fisica_cl.sh
+++ fisica_cl.sh
@@ -3,7 +3,8 @@
 function monta_frame {
      IP_DATA=`cat packet.txt | xxd -p | tr -d \\n`
 
-     PREAMBLE='55555555555555D5' #Preamble e start frame delimiter em hexa
+     #Preamble e start frame delimiter em hexa
+     PREAMBLE='55555555555555D5'
 
      #Bytes 11-14 do pacote IP
      IPORG_OCT1=`echo $((16#$(echo $IP_DATA | cut -b25-26)))`
@@ -21,56 +22,45 @@ function monta_frame {
      IPDST=`echo "${IPDST_OCT1}.${IPDST_OCT2}.${IPDST_OCT3}.${IPDST_OCT4}"`
      echo "IP de Destino: $IPDST"
 
+     #Pegar o MAC da ORIGEM
      ifconfig > ifc.txt
 
      while read line; do
           for i in $(echo $line | tr " " "\n")
           do
-               if [ "$var" = "1" ]
-               then
+               if [ "$var" = "1" ]; then
                     MAC=$i
                     var=2
-               else
-                    a=1
                fi
 
-               if [ "$i" = "HW" ]
-               then
+               if [ "$i" = "HW" ]; then
                     var=1
-               else
-                    a=1
                fi
 
-               if [ "$var" = "2" ]
-               then
+               if [ "$var" = "2" ]; then
                     var=3
-               else
-                    a=1
-               fi
 
-               if [ "$var" = "3" ]
-               then
-                    if [ "$i" = "${IPORG_OCT1}.${IPORG_OCT2}.${IPORG_OCT3}.${IPORG_OCT4}" ]
-                    then
+               if [ "$var" = "3" ] && [ "$i" = "${IPORG_OCT1}.${IPORG_OCT2}.${IPORG_OCT3}.${IPORG_OCT4}" ]; then
                          MAC_ORG=`echo $MAC`
                     fi
-               else
-                    a=1
                fi
           done
      done < ifc.txt
 
      rm ifc.txt
 
+     #Se não encontrar o MAC de origem
      if [ -z "$MAC_ORG" ]; then
           MAC_ORG="00:00:00:00:00:00"
      fi
 
      echo "MAC da Origem: $MAC_ORG"
 
+     #Ping para poder fazer o ARP
      ping -c 1 ${IPDST_OCT1}.${IPDST_OCT2}.${IPDST_OCT3}.${IPDST_OCT4} &>/dev/null
      MAC_DST=`arp ${IPDST_OCT1}.${IPDST_OCT2}.${IPDST_OCT3}.${IPDST_OCT4} | grep -E -o -e "([A-Za-z0-9]{2}:?){6}"`
 
+     #Se não encontrar o MAC de destino
      if [ -z "$MAC_DST" ]; then
           MAC_DST="00:00:00:00:00:00"
      fi
@@ -81,9 +71,16 @@ function monta_frame {
      MAC_ORG=`echo $MAC_ORG | sed "s/://g"`
      MAC_DST=`echo $MAC_DST | sed "s/://g"`
 
+     #Montar o quadro Ethernet
      echo -n "${PREAMBLE}${MAC_DST}${MAC_ORG}0800${IP_DATA}" > frame_e.hex
+
+     #Transfroma o quadro de hexa textual para binário
      xxd -r -p frame_e.hex | tr -d \\n > frame_e.dat
+
+     #Calcula o CRC e adiciona no final do quadro
      crc32 frame_e.dat | xxd -r -p >> frame_e.dat
+
+     #Transforma o quadro de binário para binário textual
      xxd -b frame_e.dat | cut -d" " -f 2-7 | tr -d \\n | sed "s/ //g" > frame_e.txt
 
      rm frame_e.hex &> /dev/null
@@ -91,6 +88,7 @@ function monta_frame {
 }
 
 while true; do
+     #Aguarda conexão da camada superior
      echo "Esperando pacote IP..."
      nc -l 8081 > packet.txt
 
@@ -104,8 +102,11 @@ while true; do
      TMQ=`nc -l 8081`
      echo "TMQ do destino: $TMQ"
 
+     #Exibe o pacote IP no formato HEX Dump
      echo "Enviando o pacote IP:"
      xxd packet.txt
+
+     #Envia o quadro Ethernet no formato binário textual para o servidor da camada física
      nc 127.0.0.1 8080 < frame_e.txt
 
      rm frame_e.txt &> /dev/null
--- fisica_sr.sh
+++ fisica_sr.sh
@@ -1,45 +1,65 @@
 #!/bin/bash
 
+#Primeiro argumento é o TMQ
 TMQ=`echo $1`
 
+#Se não informar o TMQ
 if [ -z "$TMQ" ]; then
      echo "O TMQ deve ser informado"
      exit
 fi
 
+#Faixa de valores para o TMQ
 if [ "$TMQ" -lt "88" ] || [ "$TMQ" -gt "1542" ]; then
      echo "O TMQ deve estar entre 88 e 1542"
      exit
 fi
 
 while true; do
+     #Espera a conexão do cliente da camada física
      echo "Esperando conexão..."
      nc -l 8080 > frame_r.txt
 
+     #Armazena o pedido para verificar se é a mensagem de TMQ
      REQUEST=`cat frame_r.txt`
 
+     #Só responde com o TMQ se a mensagem for exatamente "TMQ"
      if [ "$REQUEST" == "TMQ" ]; then
           echo "Repondendo com TMQ: $TMQ"
           echo $TMQ | nc 127.0.0.1 8081
      else
+          #Caso contrário considera que é um quadro Ethernet em formato binário textual
           echo "Recebendo Frame Ethernet..."
 
+          #Separa o arquivo em dígitos hexa (4 bits)
           cat frame_r.txt | sed "s/\([0-9]\{4\}\)/\1\n/g" > frame_r.dat
 
-          rm frame_r.hex &> /dev/null
-
+          #Convertendo cada dígito hexa de binário textual para hexa textual
           while read line; do
             echo "obase=16; ibase=2; $line" | bc | tr -d \\n >> frame_r.hex
           done < frame_r.dat
 
+          #Conversão de hexa textual para binário
           xxd -r -p frame_r.hex > frame_r.dat
 
+          #Exibe o quadro Ethernet no formato HEX Dump
           xxd frame_r.dat
+
+          #sed "s/.\{44\}//" -> remove o cabeçalho (Preamble + MAC Dst + MAC Src + Ethertype = 22 bytes)
+          #sed "s/.\{8\}$//" -> remove o CRC (4 bytes)
+          cat frame_r.dat | xxd -p | tr -d \\n | sed "s/.\{44\}//" | sed "s/.\{8\}$//" > payload.hex
+          
+          #Converte o payload de hexa textual para binário
+          xxd -r -p payload.hex | tr -d \\n > payload.bin
+
+          #Entrega o pacote IP (PAYLOAD do quadro Ethernet) para a camada superior
           echo "Enviando para camada superior..."
-          #nc 127.0.0.1 porta_a_definir < frame_r.txt
+          #nc 127.0.0.1 porta_a_definir < payload.bin
 
           rm frame_r.dat &> /dev/null
           rm frame_r.hex &> /dev/null
+          rm payload.hex &> /dev/null
+          rm payload.bin &> /dev/null
      fi
 
      rm frame_r.txt &> /dev/null
