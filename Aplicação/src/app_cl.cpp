24b65d00c7ae009ba3e2ac77c80b14415882b72b
claudia alessandra <claudia alessandra>
2016-10-16 06:18:53 -0200

    Camada aplicação e exemplo arquivo html pra usar no server aplicação

--- Aplicação/src/app_cl.cpp
+++ Aplicação/src/app_cl.cpp
@@ -12,18 +12,21 @@
 
 using namespace std;
 
-#define PORTA 8081
+#define PORTA_SERVER 8081
+#define PORTA_LOCAL 8083
 #define ENDERECO "127.0.0.1"
 
 
 int main(int argc, char **argv) {
-   int sockfd, portno, n;
-   struct sockaddr_in serv_addr;
+   int sockfd, portno, n, newsockfd;
+   struct sockaddr_in serv_addr,cli_addr;
    struct hostent *server;
+   socklen_t clilen;
+   
 
-   char buffer[256];
+   char buffer[1024];
 
-   portno = PORTA;
+   portno = PORTA_SERVER;
 
    /* Create a socket point */
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -55,12 +58,17 @@ int main(int argc, char **argv) {
       * will be read by server
    */
 
-   printf("Digite o servidor/site: ");
+   printf("Digite o servidor/pagina que deseja acessar: ");
    cin >> buffer;
    // Faz a requisição
-   char req[300] = "Get / HTTP/1.1\r\nHost: ";
-   strcat(req, buffer);
-   strcat(req, "\r\n");
+   char req[1024] = "GET / HTTP/1.1\r\nHost: ";
+    strcat(req, buffer);
+    strcat(req, "\r\n");
+    strcat(req, "User-Agent: Mozilla/5.0\r\n");
+    strcat(req, "Accept: text/html\r\n");
+    strcat(req, "Accept-Language: pt-BR,pt\r\n");
+    strcat(req, "Accept-Encoding: gzip, deflate\r\n");
+
    char *msg_resposta = prepara_mensagem(req);
 
    /* Send message to the server */
@@ -74,36 +82,61 @@ int main(int argc, char **argv) {
    }
 
    close(sockfd);
+   
+   
 
+   /*******Aguardando conexao para o retorno com o html***********/
+   /* First call to socket() function */
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
    if (sockfd < 0) {
       perror("ERROR opening socket 2");
       exit(1);
    }
-
+   
    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = PORTA_LOCAL;
    serv_addr.sin_family = AF_INET;
-   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
    serv_addr.sin_port = htons(portno);
 
-   /* Now connect to the server */
-   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR connecting 2");
+   /* Now bind the host address using bind() call.*/
+   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR on binding 2");
       exit(1);
    }
 
-   /* Now read server response */
-   bzero(buffer, 256);
-   n = read(sockfd, buffer, 255);
+   
+   cout << "cliente aguardando resposta do servidor...\n";
 
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
+   listen(sockfd, 5);
+   clilen = sizeof(cli_addr);
+
+   while (1) {
+       /* Accept actual connection from the client */
+       newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+       if (newsockfd < 0) {
+          perror("ERROR on accept");
+          exit(1);
+       }
+
+       /* If connection is established then start communicating */
+       bzero(buffer, 1024);
+       n = read(newsockfd, buffer, 1024);
+
+       if (n < 0) {
+          perror("ERROR reading from socket");
+          exit(1);
+       }
+
+       printf("Resposta do servidor: %s\n", buffer + 51);
+       
+        printf("%s\n",buffer);
+
+       close(newsockfd);
+       
    }
+    return 0;
 
-   printf("%s\n",buffer);
-
-   close(sockfd);
-   return 0;
 }
--- Aplicação/src/app_sr.cpp
+++ Aplicação/src/app_sr.cpp
@@ -11,13 +11,16 @@
 #include "camadas.h"
 
 using namespace std;
+FILE *openfile;
 
 #define PORTA 8082
 
 int main(int argc, char **argv) {
    int sockfd, newsockfd, portno;
    socklen_t clilen;
-   char buffer[256];
+   char buffer[1024];
+   char *lines;
+   char resultcode[50], *fileserver;
    struct sockaddr_in serv_addr, cli_addr;
    int  n;
 
@@ -46,12 +49,13 @@ int main(int argc, char **argv) {
    /* Now start listening for the clients, here process will
       * go in sleep mode and will wait for the incoming connection
    */
-   cout << "Servidor rodando...\n";
+   cout << "aguardando conexao...\n";
 
    listen(sockfd, 5);
    clilen = sizeof(cli_addr);
 
    while (1) {
+     cout << "Servidor rodando...\n";
        /* Accept actual connection from the client */
        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
 
@@ -61,22 +65,116 @@ int main(int argc, char **argv) {
        }
 
        /* If connection is established then start communicating */
-       bzero(buffer, 256);
-       n = read(newsockfd, buffer, 255);
+       bzero(buffer, 1024);
+       n = read(newsockfd, buffer, 1024);
 
        if (n < 0) {
           perror("ERROR reading from socket");
           exit(1);
        }
+		               
+	cout << "Requisicao recebida...\n";
+	/*
+	/****METODO DA REQUISIÇAO**/
+	char *metodo  = strtok(buffer+51, "\r\n");
+	printf( " %s\n", metodo );
+	
+	/****HOST SERVER**/
+	char *hostname  = strtok(NULL, "\r\n");	
+	hostname = strtok(hostname, " ");
+    	hostname = strtok(NULL, " ");
+	printf( "Host %s\n", hostname );
+	
+	/****FILE SERVER**/
+	/*int tam_host = strlen(hostname);
+	fileserver = NULL; 
+	for(int i=0; i < tam_host;i++)
+	  if(strcmp(hostname+i,"/" ) == 0) Esta dando um problema ao fazer essa verificaçao, entao considerei que o usuario sempre vai digitar a barra, por enquanto*/ 
+	    fileserver = strtok(hostname, "/");
+	  
+	fileserver = strtok(NULL, " ");
+	printf( " %s\n", fileserver );	
+	
+	/***Le arquivo html no servidor**/
+	if (strlen(fileserver) < 1 ) //arquivo padrao do servidor
+            strcpy(fileserver, "index.html");
+	
+	openfile = fopen (fileserver, "r");
+	if (openfile){
+	  printf( "Abriu o arquivo: %s\n",fileserver);  
+	  strcpy(resultcode, "200 OK");
+	  printf( "Resultado 200 OK \n");  
+	  
+	} else {
+	  printf( "arquivo nao existe\n");
+	  strcpy(fileserver, "404.html");
+	  strcpy(resultcode, "404 Not Found");
+	  openfile = fopen (fileserver, "r");
+	  if (openfile){
+	    printf( "abriu o arquivo \n");  
+	  }
+	}
+	
+	
+	printf( "tentou abrir o arquivo: %s\n", fileserver );
+	
+	char  linha[300] = "", html[1024] = "";
+	while (fgets(linha,300, openfile)!= NULL)
+	{        
+	    strcat(html, linha);          // append the new data
+	}
+	fclose(openfile);
 
-       printf("Here is the message: %s\n", buffer + 51);
+	
+	
 
-       /* Write a response to the client */
-       // Código de ver o que é a resposta
-       char resposta[] = "Mensagem de volta";
-       char *msg_resposta = prepara_mensagem(resposta);
+	/****Monta o resultado para retorno******************/
+	char result[1024];
+	strcpy(result, "Location: http://");
+	strcat(result, hostname);
+	strcat(result, "\r\n");
+	
+	strcat(result, "Date: ");
+	strcat(result, __DATE__);
+	strcat(result, __TIME__);
+	strcat(result, "\r\n");
+	
+	strcat(result, "Server: servidorteste/1.0\r\n");
+	
+	strcat(result, "Content-Type: text/html\r\n");
+	//strcat(result, tipo);
+	
+	strcat(result, "Content-Length: ");
+	//strcat(result, fileLength);
+	strcat(result, "\r\n");
+	
+	strcat(result, "Connection: close\r\n\r\n");
+	
+	
+	strcat(result, "\r\n");
+	
+	/***Conteudo HTML****/
+	strcat(result, "\r\n");
+	strcat(result, html);
+	
+	
+	/*
+	 * falta pegar os outros parametros
+	char *agent = strtok(NULL, "\r\n");
+	printf( " %s\n", agent );
+	agent = strtok(agent, " ");
+	printf( " %s\n", agent );
+	agent = strtok(agent, " ");
+	printf( " %s\n", agent );
+	
+	*/
+	
+    	
 
-       n = write(newsockfd, msg_resposta, strlen(resposta) + 52);
+       /* Write a response to the client */       
+       char *msg_resposta = prepara_mensagem(result);
+
+       n = write(newsockfd, msg_resposta, strlen(result) + 52);
 
        delete[] msg_resposta;
 
@@ -86,6 +184,7 @@ int main(int argc, char **argv) {
        }
 
        close(newsockfd);
+       cout << "Requisicao respondida...\n";
    }
 
    return 0;
